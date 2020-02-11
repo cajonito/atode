a3199f17b44ca1719359f598e6b41a04d9adf9f8
nkigs <nkitgamesake@gmail.com>
2020-02-11 17:01:03 +0900

    main.tsにgas上に無い関数が存在するとgasでエラーが起きるのでローカル関数を分離 #1

--- src/main.ts
+++ src/main.ts
@@ -3,38 +3,9 @@ import { OutputApiFactory } from './OutputApiFactory';
 
 declare var global: any;
 
-global.doPost = function(e: any) {
+global.doPost = function (e: any) {
 	const outputApiFactory = new OutputApiFactory();
 	const outputApiSlack = outputApiFactory.create('slack');
 	const slackBot = new SlackBot(outputApiSlack, e);
 	slackBot.do();
 };
-
-global.test = () => {
-	const outputApiFactory = new OutputApiFactory();
-	const outputApiConsole = outputApiFactory.create('consle');
-	const e = getTestData();
-	const slackBot = new SlackBot(outputApiConsole, e);
-	slackBot.do();
-};
-
-var getTestData = () => {
-	const fs = require('fs');
-	let contents = fs.readFileSync('./tests/data/test.json');
-
-	return {
-		parameter: {},
-		contextPath: '',
-		contentLength: 0, //dummy
-		queryString: '',
-		parameters: {},
-		postData: {
-			type: 'application/json',
-			length: 0, //dummy
-			contents: JSON.stringify(JSON.parse(contents)),
-			name: 'postData'
-		}
-	};
-};
-
-global.test();
