015595ad2429c0d03160d57deac82bbef3972f44
nkigs <nkitgamesake@gmail.com>
2020-02-14 18:24:34 +0900

    デバッグメッセージをデバッグチャンネルに送信するように #3

--- src/OutputApi/Slack.ts
+++ src/OutputApi/Slack.ts
@@ -6,7 +6,7 @@ export class Slack extends OutputApi {
     const url = 'https://slack.com/api/chat.postMessage';
     const contentType = 'application/json; charset=utf-8';
     const token = PropertiesService.getScriptProperties().getProperty('SLACK_TOKEN');
-    const channel = PropertiesService.getScriptProperties().getProperty('SLACK_CHANNEL_ID');
+    const channel = PropertiesService.getScriptProperties().getProperty('SLACK_DEBUG_CHANNEL_ID');
 
     if (token == null || channel == null) return;
 
